e7efcf7ee3dda214aca4b5009a07524c94755814
Diogo2b <dioguinho2b@gmail.com>
2024-06-06 17:05:05 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 import { defineConfig } from 'astro/config';
+=======
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+>>>>>>> parent of f179b9a (Update astro.config.mjs)
+=======
+import { defineConfig } from 'astro/config';
+>>>>>>> parent of 65ba992 (Update astro.config.mjs)
 
 // https://astro.build/config
 export default defineConfig({});
