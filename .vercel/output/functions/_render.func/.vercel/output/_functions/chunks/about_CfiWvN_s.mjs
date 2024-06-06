16ea256903b78e39a5037d8b096740bf8c4de327
Diogo2b <dioguinho2b@gmail.com>
2024-06-06 16:48:04 +0200

    revert

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_CfiWvN_s.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_CfiWvN_s.mjs
@@ -1,5 +1,5 @@
 export { renderers } from '../renderers.mjs';
 
-const page = () => import('./pages/work_DwizxepN.mjs');
+const page = () => import('./pages/about_DLHjNlRk.mjs');
 
 export { page };
