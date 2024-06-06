fd2b5827af42f1663c7706859c749f4837a3ddbe
Diogo2b <dioguinho2b@gmail.com>
2024-06-06 16:45:34 +0200

    revert

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/404_B34Y364G.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_nJR9KWHm.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/va2_L0HkD2-C.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./va2_COw4PbUv.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
