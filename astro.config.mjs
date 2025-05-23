986b2274946254084b66253c2f22fa90e679f3c2
Diogo2b <dioguinho2b@gmail.com>
2025-05-23 13:52:39 +0200

    build: purgecss ativo e lazy loading aplicado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      postcss: "./postcss.config.cjs",
+    },
+  },
+});
