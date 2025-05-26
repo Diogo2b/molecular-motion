e209f2c199bad11a1c69b29c830594e6e3de54d8
Diogo2b <dioguinho2b@gmail.com>
2025-05-26 16:57:03 +0200

    fix: corriger l'intégration de Google Analytics avec des déclarations de type global et améliorer le chargement des scripts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+export {};
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag?: (...args: any[]) => void;
+  }
+}
